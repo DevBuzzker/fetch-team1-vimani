650f5eee85e34c820353fc03be08b14c69eeaa00
Nikita <n.v.biryukov@gmail.com>
2026-05-22 14:54:31 +0200

    added getPokemonAll.js and ...Single.js

---
+++ getPokemonAll.js
@@ -0,0 +1,33 @@
+import { getPokemonSingle } from "./getPokemonSingle.js";
+import { getPokemonSingleReduced } from "./getPokemonSingle.js";
+
+// var allPokeNameUrl = await getPokemonAll();
+// console.log(allPokeNameUrl);
+// var allWithInfos = await getPokemonAllWithInfos(10, 20);
+// console.log(allWithInfos);
+
+// gives 1350 objects like
+// {name:"bulbasaur", url:"https://pokeapi.co/api/v2/pokemon/1/"}
+async function getPokemonAll() {
+  // Es gibt 1350 total.
+  var res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=9999");
+  var json = await res.json();
+  return json.results;
+  // fetch("https://pokeapi.co/api/v2/pokemon?limit=9999")
+  //   .then((res) => res.json())
+  //   .then((json) => console.log(json.results));
+}
+
+export async function getPokemonAllWithInfos(startIndex, endIndex) {
+  const allNoInfos = await getPokemonAll();
+  // console.log(allNoInfos);
+  const allWithInfos = [];
+  // let i = startIndex;
+  for (let i = startIndex; i < endIndex; i++) {
+    // console.log(pokemonNoInfo.name);
+    const pokemonNoInfo = allNoInfos[i];
+    var pokemonWithInfo = await getPokemonSingleReduced(pokemonNoInfo.url);
+    allWithInfos.push(pokemonWithInfo);
+  }
+  return allWithInfos;
+}
