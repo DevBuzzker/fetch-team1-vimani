d21d16be0bb22c7fe9c1504b602eb63b90ff68ea
Nikita <n.v.biryukov@gmail.com>
2026-05-26 16:58:22 +0200

    align images and texts to center

--- showPokemonCard.js
+++ showPokemonCard.js
@@ -7,11 +7,18 @@ export function showPokemonCard(pokemon) {
   div.classList.add("m-2");
   div.classList.add("p-2");
   div.classList.add("w-40");
-  div.classList.add("text-center");
+
+  // align images and texts to center
+  div.classList.add("flex");
+  div.classList.add("flex-col");
+  div.classList.add("items-center");
+  div.classList.add("justify-center");
+
   div.classList.add("flex-shrink-0");
 
   const img = document.createElement("img");
   img.src = pokemon.image;
+  img.classList.add("max-h-30");
   div.appendChild(img);
 
   const titlePara = document.createElement("p");
